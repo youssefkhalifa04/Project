bea690fb21dbb9c8ea858165cf732de5f9bbd180
Youssef <youssefkh.tv2@gmail.com>
2025-04-13 22:22:33 +0100

    .

--- backend/signup.php
+++ backend/signup.php
@@ -4,15 +4,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = htmlspecialchars($_POST["email"]);
     $password = htmlspecialchars($_POST["password"]);
     $confirmPassword = htmlspecialchars($_POST["confirmPassword"]);
-    try{
-        require_once("dbh.inc.php");
-    }
-    catch( PDOException $e){
-        die("Connection failed: " . $e->getMessage());
-    }
+    
     if (!empty($name) && !empty($email) && !empty($password) && !empty($confirmPassword) &&  $password === $confirmPassword) {
         // Hash the password
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    }
+    else if (empty($name) || empty($email) || empty($password) || empty($confirmPassword)) {
+        echo "please fill all the fields";
+    }
+    else if ($password !== $confirmPassword) {
+        echo "passwords do not match";
+    }
+    try{
+        require_once("dbh.inc.php");
+        $query = "INSERT INTO users (name, email, password) VALUES (`$name`, `$email`, `$password`)";
+    }
+    catch( PDOException $e){
+        die("Connection failed: " . $e->getMessage());
     }
 }
 else{
